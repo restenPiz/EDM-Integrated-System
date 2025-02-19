ff7277a12fa1d67704b6076d462939e4c2fa4673
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-02-19 10:41:49 +0200

    Creating a PTEDM Component and sync with my routes

---
+++ app/Livewire/PTEDM.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class PTEDM extends Component
+{
+    public function render()
+    {
+        return view('livewire.p-t-e-d-m');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,9 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
+//*Start with the main pages route
+Volt::route('/PTEDM', 'p-t-e-d-m.blade.php')
+    ->middleware(['auth'])
+    ->name('PTEDM');
+
 require __DIR__ . '/auth.php';
