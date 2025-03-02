0afdb6378cac4f835924f8eda301104585cf35e0
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-03-02 21:18:23 +0200

    Some updates in the aplication

--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -55,38 +55,9 @@
                                         </div>
                                     </div>
                                     <!--end col-->
-                                    <div class="col-xl-6">
+                                    <div class="col" style="text-align: right">
                                         <div class="row g-3">
-                                            <div class="col-sm-4">
-                                                <div class="">
-                                                    <input type="text" class="form-control" id="datepicker-range"
-                                                        data-provider="flatpickr" data-date-format="d M, Y"
-                                                        data-range-date="true" placeholder="Select date">
-                                                </div>
-                                            </div>
-                                            <!--end col-->
-                                            <div class="col-sm-4">
-                                                <div>
-                                                    <select class="form-control" data-plugin="choices" data-choices
-                                                        data-choices-search-false name="choices-single-default"
-                                                        id="idStatus">
-                                                        <option value="">Status</option>
-                                                        <option value="all" selected>All</option>
-                                                        <option value="Active">Active</option>
-                                                        <option value="Block">Block</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <!--end col-->
-
-                                            <div class="col-sm-4">
-                                                <div>
-                                                    <button type="button" class="btn btn-primary w-100"
-                                                        onclick="SearchData();"> <i
-                                                            class="ri-equalizer-fill me-2 align-bottom"></i>Filters</button>
-                                                </div>
-                                            </div>
-                                            <!--end col-->
+                                            
                                         </div>
                                     </div>
                                 </div>
