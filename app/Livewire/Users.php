eacc245978d15385ae8e9daf17fbdb2f1214e40e
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-03-02 20:32:55 +0200

    Some updates in the application

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -2,12 +2,40 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class Users extends Component
 {
+    public $name, $id, $file, $password, $email;
     public function render()
     {
         return view('livewire.users');
+    }
+    public function save()
+    {
+        $this->validate([
+            'name' => 'required|string|max:255',
+            'file' => 'required|string|max:255',
+            'password' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+        ]);
+
+        $response = Http::post(env('API_URL') . '/storeUser', [
+            'name' => $this->name,
+            'file' => $this->file,
+            'password' => $this->password,
+            'email' => $this->email,
+        ]);
+
+        if ($response->successful()) {
+            session()->flash('success', 'User added with success!');
+            $this->reset(['name', 'file', 'email', 'password']);
+            $this->dispatch('hide-alerts');
+            $this->fetchPts();
+        } else {
+            session()->flash('error', 'Failed to add User!');
+            $this->dispatch('hide-alerts');
+        }
     }
 }
